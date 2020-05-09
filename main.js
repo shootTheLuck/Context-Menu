841bad1e29eb1a67bd75811221a8fb10d0b83b25
Shoot the Luck! <29633714+shootTheLuck@users.noreply.github.com>
2020-05-09 19:54:15 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+
+import ContextMenu from "./ContextMenu.js";
+
+var test = document.getElementById("test");
+var feedback = document.getElementById("feedback");
+
+var contextMenu = new ContextMenu({handleMouseEvents: false});
+contextMenu.addMenuOption("Edit");
+contextMenu.addMenuOption("Back");
+contextMenu.addMenuOption("Create");
+
+contextMenu.addEventListener("selection", function(evt) {
+    feedback.innerText = "You selected: " + evt.detail.value;
+    console.log("contextMenuSelect:", evt.detail.value);
+    if (evt.detail.value === "Edit") {
+        // transformControls.show();
+    }
+});
+
+test.appendChild(contextMenu);
+
+// use something like this if handleMouseEvents = false
+test.addEventListener("contextmenu", function(evt) {
+    if (evt.ctrlKey) {
+        contextMenu.show(evt, "New Line");
+    } else {
+        contextMenu.show(evt);
+    }
+});
+
+document.addEventListener("keydown", function(evt) {
+    if (evt.key === "t") {
+        contextMenu.removeMenuOption("Create");
+    }
+    if (evt.key === "m") {
+        contextMenu.disableItem("Create");
+    }
+});
+
+document.addEventListener("mousedown", function(evt) {
+    // alert();
+    // console.log("mousedown");
+});
+
+
+
+
+
