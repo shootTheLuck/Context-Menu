fc461c5562e3aa2480c061552828d0ea44b64ac1
Shoot the Luck! <29633714+shootTheLuck@users.noreply.github.com>
2020-05-10 16:29:43 +0000

    Listen for rightClickEvent

--- main.js
+++ main.js
@@ -5,30 +5,43 @@ var rectangleElement = document.getElementById("rectangle");
 var squareElement = document.getElementById("square");
 var feedback = document.getElementById("feedback");
 
-var contextMenu = new ContextMenu({handleMouseEvents: false});
+var contextMenu = new ContextMenu({autoDisplay: true});
 contextMenu.addMenuOption("Edit");
 contextMenu.addMenuOption("Back");
 contextMenu.addMenuOption("Create");
 rectangleElement.appendChild(contextMenu);
 
-var target = null;
 
-contextMenu.addEventListener("selection", function(evt) {
-    feedback.innerText = "You selected: " + evt.detail.value;
-    console.log("contextMenuSelect:", evt.detail.value);
-    if (evt.detail.value === "Change Color") {
-        target.style.backgroundColor = "blue";
+// use something like this if handleMouseEvents = false
+contextMenu.addEventListener("rightClick", function(evt) {
+    let targetElement = evt.detail.value.target;
+    console.log("contextMenu right click on html target:", targetElement);
+    if (targetElement === squareElement) {
+        contextMenu.show(evt.detail.value, "Change Color");
+    } else {
+        contextMenu.show(evt.detail.value);
     }
 });
 
 // use something like this if handleMouseEvents = false
-rectangleElement.addEventListener("contextmenu", function(evt) {
-    if (evt.target === squareElement) {
-        contextMenu.show(evt, "Change Color");
-        target = squareElement;
-    } else {
-        contextMenu.show(evt);
-        target = null;
+// rectangleElement.addEventListener("contextmenu", function(evt) {
+    // if (evt.target === squareElement) {
+        // contextMenu.show(evt, "Change Color");
+    // } else {
+        // contextMenu.show(evt);
+    // }
+// });
+
+contextMenu.addEventListener("selection", function(evt) {
+    let selectedValue = evt.detail.value;
+    let targetElement = evt.detail.initialClick.target;
+
+    feedback.innerText = "You selected: " + selectedValue;
+    console.log("contextMenu selection:", selectedValue);
+    console.log("contextMenu original target:", targetElement);
+
+    if (targetElement === squareElement && selectedValue === "Change Color") {
+        targetElement.style.backgroundColor = "blue";
     }
 });
 
